c6a82de5798ed2777552a92500899bb2c20789c4
uropeng <uropeng@mit.edu>
2022-05-16 15:54:33 -0400

    Robot

--- scripts/panda.py
+++ scripts/panda.py
@@ -1,12 +1,15 @@
 import rospy
 from franka_interface import ArmInterface
-import RRT
 
 if __name__ == '__main__':
     rospy.init_node('testing_node')
     arm = ArmInterface()
-    q_start = arm.convertToList(arm.joint_angles)
+    q_start = arm.convertToList(arm.joint_angles())
     q_end = list(q_start)
     q_end[1] += 0.2
     q_end[0] += 0.4
-    arm.execute_position_path([q_start, q_end])
+    q_end[2] -= 0.2
+    q_end = arm.convertToDict(q_end)
+    q_start = arm.convertToDict(q_start)
+    path = [q_start, q_end]
+    arm.execute_position_path(path)
