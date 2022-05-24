ebf377bf5000c8a5516cfab893ff40067556840f
uropeng <uropeng@mit.edu>
2022-05-24 18:45:08 -0400

    Fixed some pybullet

--- scripts/panda.py
+++ scripts/panda.py
@@ -3,13 +3,19 @@ from franka_interface import ArmInterface
 import table_env
 from Grasp import Grasp
 import numpy
+import pb_robot
 
 if __name__ == '__main__':
+    pb_robot.utils.connect(use_gui=True)
+    pb_robot.utils.disable_real_time()
+    pb_robot.utils.set_default_camera()
+
     rospy.init_node('testing_node')
     arm = ArmInterface()
-    objects, floor, _ = table_env.execute()
+    objects, floor, robot = table_env.execute()
 
     grasp = Grasp(robot, objects)
+
     arm.hand.open()
     motion = RRT(robot)
     new_path = None
@@ -17,10 +23,10 @@ if __name__ == '__main__':
     while new_path is None:
         grasp_pose, q_grasp = grasp.grasp('plate')
         relative_grasp = numpy.dot(numpy.linalg.inv(objects['plate'].get_transform()), grasp_pose)
-        print(relative_grasp)
+       # print(relative_grasp)
         grasp_in_world = numpy.dot(objects['plate'].get_transform(), relative_grasp)
         conf = robot.arm.ComputeIK(grasp_in_world)
-        print(q_grasp, conf)
+       # print(q_grasp, conf)
         new_path = motion.motion(q_start, conf)
 
     final_path = []
@@ -28,4 +34,4 @@ if __name__ == '__main__':
         final_path.append(arm.convertToDict(q))
     print(final_path)
     # arm.execute_position_path(final_path)
->>>>>>> 4b440a135ac8cd4811294b6b629649b3dded08d7
+
