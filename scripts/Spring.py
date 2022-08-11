a7d76e0a2f8e025e6876e37a158b50ac8751e70c
Rachel Lu <lur@MIT.EDU>
2022-08-11 10:43:08 -0400

    Edits to stiffness

--- scripts/Spring.py
+++ scripts/Spring.py
@@ -8,29 +8,27 @@ import util
 import numpy
 import vobj
 
-
-def get_force(distance, stiffness):
-    return -1 * numpy.linalg.norm(numpy.dot(stiffness, distance))
-
-
-def get_distance_from_force(force, stiffness):
-    """
-    :param force: Force in Newtons
-    :return: Linear distance to apply specified force in meters
-    """
-    return -1 * numpy.linalg.norm(numpy.dot(force, numpy.linalg.inv(stiffness)))
-
-
 class Spring:
     def __init__(self, robot, arm):
         self.arm = arm
         self.robot = robot
+        self.stiffness = [1, 1, 1, 1, 1, 1, 1]
+
+    def get_force(self, distance):
+        return -1 * numpy.linalg.norm(numpy.dot(self.stiffness, distance))
+
+    def get_distance_from_force(self, force):
+        """
+        :param force: Force in Newtons
+        :return: Linear distance to apply specified force in meters
+        """
+        return -1 * numpy.linalg.norm(numpy.dot(force, numpy.linalg.inv(self.stiffness)))
 
     def apply_force(self, force):
         """
         :param force: Force in Newtons
         """
-        distance = get_distance_from_force(force)
+        distance = self.get_distance_from_force(force)
         new_q = self.q_from_distance(distance)
         print(new_q)
         if new_q:
